38452bd8ef77911f4b8f8a548742476bc8e6e7da
T Bhanu Prakash <tbhanu.prakash@pacteraedge.com>
2023-06-14 09:47:24 +0000

    Added main.tf

---
+++ Terraform_Scripts/Databases/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources32e353"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "exampl54esa537823"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "examples5qlserver460"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "test" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = true
+
+  tags = {
+    foo = "bar"
+  }
+}
