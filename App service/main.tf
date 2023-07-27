c26aeea53f9a5351a9b787d98dc6842125453315
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-07-27 07:38:17 +0000

    Added main.tf

---
+++ App service/main.tf
@@ -0,0 +1,37 @@
+# main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+    # Additional configurations can be added here
+  }
+
+  app_settings = {
+    "WEBSITE_NODE_DEFAULT_VERSION" = "14"
+    # Additional app settings can be added here
+  }
+}
