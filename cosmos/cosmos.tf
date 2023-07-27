bfb211dd70b3a4ca9da4f6563f602bb6bb00869a
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-07-27 14:59:03 +0000

    Updated cosmos.tf

--- cosmos/cosmos.tf
+++ cosmos/cosmos.tf
@@ -1,16 +1,18 @@
+
+
 provider "azurerm" {
   features {}
 }
 
 # Resource Group
-data "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resource-group232"
-  #location = "East US"  # Update with your desired location
+  location = "East US"  # Update with your desired location
 }
 
 # Cosmos DB Account
 resource "azurerm_cosmosdb_account" "cosmosdb" {
-  name                = "my-cosmosdb-account"
+  name                = "my-cosmosdb-account1"
   location            = "East US"
   resource_group_name = "example-resource-group232"
   offer_type          = "Standard"  # Change to desired offer type (e.g., "Standard", "Autoscale", "Serverless")
@@ -18,6 +20,11 @@ resource "azurerm_cosmosdb_account" "cosmosdb" {
 
   consistency_policy {
     consistency_level = "Session"  # Change to desired consistency level (e.g., "BoundedStaleness", "Eventual", etc.)
+  }
+
+  geo_location {
+    location          = "East US"  # Replace with the desired location for data replication
+    failover_priority = 0
   }
 
   tags = {
@@ -28,3 +35,4 @@ resource "azurerm_cosmosdb_account" "cosmosdb" {
 output "cosmosdb_account_endpoint" {
   value = azurerm_cosmosdb_account.cosmosdb.document_endpoint
 }
+
