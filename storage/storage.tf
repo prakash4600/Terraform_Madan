efbbbc0ed7389ffa8fb10d606cf25b683df652fd
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-07-27 17:55:18 +0000

    Updated storage.tf

--- storage/storage.tf
+++ storage/storage.tf
@@ -3,16 +3,16 @@ provider "azurerm" {
 }
 
 # Resource Group
-data "azurerm_resource_group" "example" {
-  name     = "example-resource-group232"
-  #location = "East US"  # Update with your desired location
+resource "azurerm_resource_group" "storage_rg" {
+  name     = "my-storage-account-rg"
+  location = "East US"  # Update with your desired location
 }
 
 # Storage Account
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "mystorageaccount232"  # Update with a unique name
-  resource_group_name      = "example-resource-group232"
-  location                 = "East US"
+  name                     = "mystorageaccount"  # Update with a unique name
+  resource_group_name      = azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -21,16 +21,24 @@ resource "azurerm_storage_account" "storage_account" {
   }
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "mycontainer232"
+# Storage Container
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "mycontainer"  # Update with a unique name for the container
   storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "private"  # Change this to "blob" if you want public read access
+  container_access_type = "private"  # Change to "blob" if you want the container to be publicly accessible
+
+  depends_on = [azurerm_storage_account.storage_account]
 }
 
 output "storage_account_name" {
   value = azurerm_storage_account.storage_account.name
 }
 
-output "container_name" {
-  value = azurerm_storage_container.container.name
+output "storage_account_primary_key" {
+  value     = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.storage_container.name
 }
