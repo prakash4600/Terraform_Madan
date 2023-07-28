22aa3a3e81c5df8ecbc6e6353aef5d2bf9bb263d
T Bhanu Prakash <tbhanu.prakash@pacteraedge.com>
2023-07-28 12:29:25 +0000

    Added main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "storage" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.storage.name
+  location                 = azurerm_resource_group.storage.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"  # Change this to "blob" if you want public read access
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
+}
