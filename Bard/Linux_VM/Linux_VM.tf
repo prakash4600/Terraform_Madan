217070c073e494dd73a25dc8e544576b0e6835d8
T Bhanu Prakash <tbhanu.prakash@pacteraedge.com>
2023-07-31 06:42:25 +0000

    Updated Linux_VM.tf

--- Bard/Linux_VM/Linux_VM.tf
+++ Bard/Linux_VM/Linux_VM.tf
@@ -5,6 +5,15 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 }
 
+variable "479266ef-b2aa-4dc8-8c29-07d76744d6bc" {}
+variable "d6dc57fd-b38b-4289-a7a5-3c758257795c" {}
+variable "WtF8Q~-0amljSHR~VCudza~8BX1ViYlBjFx23dgt" {}
+variable "798b4769-c7e2-489c-98b8-362ceda12432" {}
+variable "My_Terraform_Practice" {}
+variable "West US" {}
+variable "admin" {}
+variable "Admin@Password" {}
+
 resource "azurerm_resource_group" "main" {
   name = var.resource_group_name
   location = var.location
@@ -63,11 +72,3 @@ resource "azurerm_linux_virtual_machine" "main" {
   image = "debian:11"
 }
 
-variable "479266ef-b2aa-4dc8-8c29-07d76744d6bc" {}
-variable "d6dc57fd-b38b-4289-a7a5-3c758257795c" {}
-variable "WtF8Q~-0amljSHR~VCudza~8BX1ViYlBjFx23dgt" {}
-variable "798b4769-c7e2-489c-98b8-362ceda12432" {}
-variable "My_Terraform_Practice" {}
-variable "West US" {}
-variable "admin" {}
-variable "Admin@Password" {}
