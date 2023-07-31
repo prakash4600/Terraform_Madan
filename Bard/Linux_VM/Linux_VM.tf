8cc0e0179b57237bace58dbd3faecf0986889693
T Bhanu Prakash <tbhanu.prakash@pacteraedge.com>
2023-07-31 06:46:21 +0000

    Updated Linux_VM.tf

--- Bard/Linux_VM/Linux_VM.tf
+++ Bard/Linux_VM/Linux_VM.tf
@@ -5,14 +5,7 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 }
 
-variable "479266ef-b2aa-4dc8-8c29-07d76744d6bc" {}
-variable "d6dc57fd-b38b-4289-a7a5-3c758257795c" {}
-variable "WtF8Q~-0amljSHR~VCudza~8BX1ViYlBjFx23dgt" {}
-variable "798b4769-c7e2-489c-98b8-362ceda12432" {}
-variable "My_Terraform_Practice" {}
-variable "West US" {}
-variable "admin" {}
-variable "Admin@Password" {}
+
 
 resource "azurerm_resource_group" "main" {
   name = var.resource_group_name
