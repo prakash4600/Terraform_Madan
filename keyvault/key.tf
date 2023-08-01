69b8c23e1fa59de3162c5ca517b873a1fb84a773
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-08-01 05:53:39 +0000

    Updated key.tf

--- keyvault/key.tf
+++ keyvault/key.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                        = "example-keyvault"
+  name                        = "example-keyvault22"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   sku_name                    = "standard"   # Specify the SKU name (e.g., "standard", "premium")
