e90db67bb48b4eddca3316f3e3ae135d46c5d5d3
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-08-08 09:31:11 +0000

    Updated tf.tf

--- peering/tf.tf
+++ peering/tf.tf
@@ -9,28 +9,30 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
-  address_space       = ["183.0.0.0/16"]
+  address_space       = ["187.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_virtual_network" "vnet2" {
   name                = "vnet2"
-  address_space       = ["183.1.0.0/16"]
+  address_space       = ["187.1.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_virtual_network_peering" "peering" {
+resource "azurerm_virtual_network_peering" "peering_vnet1_to_vnet2" {
   name                 = "vnet1-to-vnet2"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
 
-  remote_virtual_network {
-    id = azurerm_virtual_network.vnet2.id
-  }
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
 }
 
-output "peering_id" {
-  value = azurerm_virtual_network_peering.peering.id
+resource "azurerm_virtual_network_peering" "peering_vnet2_to_vnet1" {
+  name                 = "vnet2-to-vnet1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
 }
