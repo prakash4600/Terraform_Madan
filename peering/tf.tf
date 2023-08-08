68d39326214f8986e24728e991f03a961c18d373
Koritala Vasanthi <koritala.vasanthi@pacteraedge.com>
2023-08-08 09:28:21 +0000

    Updated tf.tf

--- peering/tf.tf
+++ peering/tf.tf
@@ -3,17 +3,34 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources234"
+  name     = "example-resources"
   location = "East US"
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "example-log-analytics"
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = ["183.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku                 = "PerGB2018"
 }
 
-output "workspace_id" {
-  value = azurerm_log_analytics_workspace.example.workspace_id
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet2"
+  address_space       = ["183.1.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network_peering" "peering" {
+  name                 = "vnet1-to-vnet2"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+
+  remote_virtual_network {
+    id = azurerm_virtual_network.vnet2.id
+  }
+}
+
+output "peering_id" {
+  value = azurerm_virtual_network_peering.peering.id
 }
